e2aebdab68e684078a4e535bcea2284145b6f27b
Rodri-Ramos1 <ramos.rodrii.13@gmail.com>
2024-09-15 12:49:29 -0300

    clase 6

---
+++ clase 6/ejercicio1.py
@@ -0,0 +1,4 @@
+import operaciones
+
+global2 = operaciones.suma(5,6)
+print(operaciones.suma(5,6))
---
+++ clase 6/operaciones.py
@@ -0,0 +1,29 @@
+
+import math
+def suma(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+def resta(numero1, numero2):
+    resultado = numero1 - numero2
+    return resultado
+
+def multiplicar(numero1, numero2):
+    resultado = numero1 * numero2
+    return resultado
+
+def division(numero1, numero2):
+    resultado = numero1 / numero2
+    return resultado
+
+def potencia(numero, exponente):
+    resultado = numero ** exponente
+    return resultado
+
+def raizCuadrada(numero):
+    resultado = math.sqrt(numero)
+    return resultado
+
+def seno(numero):
+    resultado = math.sin(numero)
+    return resultado
